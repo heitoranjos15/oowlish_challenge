5daa1d9b142acf81179b6418a3b13852de4140b1
heitor.anjos.nex <heitor.anjos@nextel.com.br>
2020-11-06 14:32:16 -0300

    Customer Model created

--- customer/api/models.py
+++ customer/api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    gender = models.CharField(max_length=1)
+    company = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    geo_latitude = models.FloatField()
+    geo_longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
