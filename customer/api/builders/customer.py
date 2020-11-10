2249ee803c60f5287d6787c5f7d1c2cfb66df39f
heitor.anjos.nex <heitor.anjos@nextel.com.br>
2020-11-10 13:19:22 -0300

    Adjust methods from builders/customer.py to private

--- customer/api/builders/customer.py
+++ customer/api/builders/customer.py
@@ -4,7 +4,7 @@ from aiohttp import ClientSession
 from integrations.google_geo_coding import get_geo_code_by_address_async
 
 
-async def get_geo_location(address, session):
+async def __get_geo_location(address, session):
     try:
         geo_data = await get_geo_code_by_address_async(address, session)
         geo_location = geo_data[0].get('geometry').get('location')
@@ -16,19 +16,19 @@ async def get_geo_location(address, session):
         pass
 
 
-async def set_geo_location(row, session):
-    row.pop('id', None)
-    geo_location = await get_geo_location(row.get('city'), session)
+async def __set_geo_location(row, session):
+    geo_location = await __get_geo_location(row.get('city'), session)
     if geo_location:
         row.update(geo_location)
     return row
 
 
-async def parse_csv_data(csv_data):
+async def __parse_csv_data(csv_data):
     async with ClientSession() as session:
         tasks = list()
         for row in csv_data:
-            tasks.append(set_geo_location(row, session))
+            row.pop('id', None)
+            tasks.append(__set_geo_location(row, session))
         return await asyncio.gather(*tasks)
 
 
@@ -48,4 +48,4 @@ def build_customer(csv_data):
     list
         Dict with all customer data
     """
-    return asyncio.run(parse_csv_data(csv_data))
+    return asyncio.run(__parse_csv_data(csv_data))
